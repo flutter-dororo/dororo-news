1630f58ebc87116404f889a63910173468331132
zhangjianlei <djkloopjs@gmail.com>
2019-06-24 15:37:31 +0800

    导航按钮优化

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -9,33 +9,67 @@ import 'package:dororo_news/pages/find_page/find_page.dart';
 import 'package:dororo_news/pages/home_page/home.dart';
 import 'package:dororo_news/pages/me_page/me_page.dart';
 
+/// plugins
+
 class DororoRouter extends StatefulWidget {
   @override
   _DororoRouterState createState() => new _DororoRouterState();
 }
 
 class _DororoRouterState extends State<DororoRouter>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   List<Widget> _pages = [HomePage(), FindPage(), ArticlePage(), MePage()];
 
   final _defaultColor = CupertinoColors.black;
   final _activeColor = CupertinoColors.activeGreen;
   int _currentIndex = 0;
+  bool flag = true;
+
+  AnimationController _controller;
+  AnimationController _controller1;
+  Animation<double> animation;
+  Animation<double> animation1;
 
   void initState() {
     super.initState();
+    _controller = new AnimationController(
+        duration: const Duration(milliseconds: 300), vsync: this);
+    _controller1 = new AnimationController(
+        duration: const Duration(milliseconds: 300), vsync: this);
+
+    final CurvedAnimation curve =
+        CurvedAnimation(parent: _controller, curve: Curves.linear);
+    final CurvedAnimation curve2 =
+        CurvedAnimation(parent: _controller1, curve: Curves.linear);
+
+    animation = Tween(begin: 0.75, end: 1.0).animate(curve);
+    animation1 = Tween(begin: 0.75, end: 1.0).animate(curve2);
+
+    _controller1
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        }
+      });
+
+    _controller
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller1.reverse();
+        }
+      })
+      ..forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
-
       tabBar: CupertinoTabBar(
         currentIndex: _currentIndex,
         onTap: handleTabChange,
         items: [
-          _renderNavigationBar(AntDesign.home, _defaultColor,
-              AntDesign.home, _activeColor, '首页', 0),
+          _renderNavigationBar(AntDesign.home, _defaultColor, AntDesign.home,
+              _activeColor, '首页', 0),
           _renderNavigationBar(AntDesign.profile, _defaultColor,
               AntDesign.profile, _activeColor, '文章', 1),
           _renderNavigationBar(Entypo.compass, _defaultColor, Entypo.compass,
@@ -54,26 +88,38 @@ class _DororoRouterState extends State<DororoRouter>
     );
   }
 
-  BottomNavigationBarItem _renderNavigationBar(
-      icon, Color defaultColor, activeIcon, Color activeColor, String title, int idx) {
+  BottomNavigationBarItem _renderNavigationBar(icon, Color defaultColor,
+      activeIcon, Color activeColor, String title, int idx) {
     return BottomNavigationBarItem(
       icon: Icon(
         icon,
         color: defaultColor,
       ),
-      activeIcon: Icon(
-        activeIcon,
-        color: activeColor,
+      activeIcon: ScaleTransition(
+        scale: flag ? animation : animation1,
+        child: Icon(
+          activeIcon,
+          color: activeColor,
+        ),
       ),
-      title: Text(title, style: TextStyle(
-        color: _currentIndex == idx ? activeColor : defaultColor
-      ),),
+      title: Text(
+        title,
+        style:
+            TextStyle(color: _currentIndex == idx ? activeColor : defaultColor),
+      ),
     );
   }
 
   void handleTabChange(int value) {
     setState(() {
       _currentIndex = value;
+      flag = !flag;
     });
+
+    if (flag) {
+      _controller.forward();
+    } else {
+      _controller1.forward();
+    }
   }
 }
